0429a1573e860be4417eecc8444185763d7e358e
CB-Pantomime <christopherbrown.contact@gmail.com>
2021-12-02 13:47:03 -0800

    updated styling and layout for poem index, poem edit, poem delete, confirm page, log in, sign up. Reformatted home page to display different content depending on if an auth user is logged in or not.

--- main_app/forms.py
+++ main_app/forms.py
@@ -16,3 +16,16 @@ class PoemForm(forms.ModelForm):
 
         }
 
+class UpdatePoemForm(forms.ModelForm):
+
+    class Meta:
+        model = Poem
+        fields = ('name', 'title', 'body')
+
+        widgets = {
+
+            'name': forms.TextInput(attrs={'class': 'form-control', 'id': 'name', 'placeholder': 'What is your name?', 'label':''}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'id': 'title', 'placeholder': 'What is your poem called?'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'id': 'textAreaBody', 'placeholder': 'Type your poem here...'})
+
+        }
